d4edda99516be2fc143caabc06e18d0b04283631
Michael Forrest <michael.forrest@gmail.com>
2022-05-21 21:36:19 +0100

    Create module with Swift support

---
+++ Gphoto2Swift/Gphoto2Swift.h
@@ -0,0 +1,18 @@
+//
+//  Gphoto2Swift.h
+//  Gphoto2Swift
+//
+//  Created by Michael Forrest on 21/05/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Gphoto2Swift.
+FOUNDATION_EXPORT double Gphoto2SwiftVersionNumber;
+
+//! Project version string for Gphoto2Swift.
+FOUNDATION_EXPORT const unsigned char Gphoto2SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Gphoto2Swift/PublicHeader.h>
+
+@import Gphoto2
